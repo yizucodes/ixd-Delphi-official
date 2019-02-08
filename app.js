64abd8de5db4e63136e943e2f095dcd59cc44acc
Yi Zu <zuyi@bu.edu>
2019-02-08 12:10:06 -0800

    fix route to chat page

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ if ('development' == app.get('env')) {
 
 
 app.get('/', index.view);
-app.get('/', chat.view);
+app.get('/chat', chat.view);
 // Example route
 // app.get('/users', user.list);
 
--- routes/chat.js
+++ routes/chat.js
@@ -1,7 +1,8 @@
 var data = require("../data.json");
 
-exports.view = function(request, response) {    
+exports.view = function(request, response) { 
 	// Your code goes here
 	console.log(data)
 	response.render('chat', {'name': ''});
- };
+
+};
