5a10fc8c56839da8452d265ddc7394ab95d3cfb4
Alexander Polus <alex.polus13@gmail.com>
2019-02-19 14:38:57 -0800

    Added js for chat

---
+++ public/js/chat.js
@@ -0,0 +1,38 @@
+//To manipulate HTML
+//Need to include this
+
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	$("#testjs").click(function(e) {
+		$('.jumbotron h1').text("Javascript has taken control");
+        $("#testjs").text("Please wait...");
+        $(".jumbotron p").addClass("active");
+	});
+
+    $("a.thumbnail").click(projectClick);
+	// Add any additional listeners here
+	// example: $("#div-id").click(functionToCall);
+}
+
+function projectClick(e) {
+    e.preventDefault();
+    $(this).css("background-color", "#7fff00");
+    
+    //New Stuff Lab 3
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+        $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+        $(description).fadeToggle();
+    }
+}
