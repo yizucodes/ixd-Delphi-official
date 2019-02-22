26ae30b503b4e646af9358c868da980f15cb8083
Alexander Polus <alex.polus13@gmail.com>
2019-02-21 20:38:45 -0800

    Changed send button

--- public/js/chat.js
+++ public/js/chat.js
@@ -6,12 +6,15 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
-})
+});
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-    //
+    $("#userSub").on("click", function(){
+    var umsg = ($("#usermsg").val());
+    $("#newText").append("<p>"+umsg+"</p>");
+    $("#usermsg").val("");
 }
 
 function addMessage() {
