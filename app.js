586ca0a9e16714ccc24e302c051cb07633d870c4
Alexander Polus <alex.polus13@gmail.com>
2019-03-01 10:56:52 -0800

    Got meme css&js working

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.get('/', index.view);
 app.get('/chat', chat.view);
 
 //Version b
-app.get('/chat/b', chat.view2);
+app.get('/chatb', chat.view2);
 // Example route
 // app.get('/users', user.list);
 
